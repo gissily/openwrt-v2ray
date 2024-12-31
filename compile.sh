46ee0232aa342ce0b1a96cad32e1ea14fc766d7f
gissily <gissily@outlook.com>
2025-01-01 03:42:52 +0800

    fix naming

--- compile.sh
+++ compile.sh
@@ -116,7 +116,7 @@ ls -la ./
 apks=`ls ./*.apk`
 for apkname in $apks
 do
-   name=$(echo $apkname | sed 's/\..*$//')
+   name=$(echo $apkname | sed -E 's/(([^.]*\.){2}[^.]*)\..*/\1/')
    newname="${name}_${ARCH}.apk"
    mv ${apkname} ${newname}
 done
